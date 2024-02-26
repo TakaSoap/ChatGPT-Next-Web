a61bb87813b6c237ffcfe2d6646955ff76b359d9
TakaSoap <34995160+TakaSoap@users.noreply.github.com>
2024-02-26 18:51:19 +0800

    Update config.ts

--- app/store/config.ts
+++ app/store/config.ts
@@ -39,23 +39,23 @@ export const DEFAULT_CONFIG = {
 
   disablePromptHint: false,
 
-  dontShowMaskSplashScreen: false, // dont show splash screen when create chat
-  hideBuiltinMasks: false, // dont add builtin masks
+  dontShowMaskSplashScreen: true, // dont show splash screen when create chat
+  hideBuiltinMasks: true, // dont add builtin masks
 
   customModels: "",
   models: DEFAULT_MODELS as any as LLMModel[],
 
   modelConfig: {
     model: "gpt-3.5-turbo" as ModelType,
-    temperature: 0.5,
+    temperature: 0.8,
     top_p: 1,
-    max_tokens: 4000,
+    max_tokens: 16384,
     presence_penalty: 0,
     frequency_penalty: 0,
     sendMemory: true,
-    historyMessageCount: 4,
-    compressMessageLengthThreshold: 1000,
-    enableInjectSystemPrompts: true,
+    historyMessageCount: 10,
+    compressMessageLengthThreshold: 2000,
+    enableInjectSystemPrompts: false,
     template: DEFAULT_INPUT_TEMPLATE,
   },
 };
